fb7c4a3f63f800fb0bcc4c06ebfb37be3b61044e
ekkertigor <ekkert.igor@gmail.com>
2016-12-11 23:27:54 +0300

    add new dump

--- taskbotDump.sql
+++ taskbotDump.sql
@@ -79,7 +79,7 @@ CREATE TABLE "Target" (
     text character varying(1000),
     list integer,
     user_t integer,
-    "idDeleted" smallint DEFAULT 0
+    is_deleted smallint DEFAULT 0
 );
 
 
@@ -110,8 +110,8 @@ CREATE TABLE "Task" (
     text character varying(1000),
     "time" character varying(100),
     repeat integer,
-    "isDone" smallint DEFAULT 0,
-    "isDeleted" smallint DEFAULT 0
+    is_done smallint DEFAULT 0,
+    is_deleted smallint DEFAULT 0
 );
 
 
@@ -138,7 +138,8 @@ ALTER TABLE public.user_id_seq OWNER TO postgres;
 CREATE TABLE "User" (
     id integer DEFAULT nextval('user_id_seq'::regclass) NOT NULL,
     chat integer,
-    name character varying(255)
+    name character varying(255),
+    user_id smallint
 );
 
 
@@ -156,7 +157,7 @@ COPY "List" (id, name, "user") FROM stdin;
 -- Data for Name: Target; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY "Target" (id, text, list, user_t, "idDeleted") FROM stdin;
+COPY "Target" (id, text, list, user_t, is_deleted) FROM stdin;
 2731403	sdds	\N	\N	0
 1	Ggfty	\N	273143403	0
 2	Dtudf	\N	273143403	0
@@ -173,7 +174,7 @@ COPY "Target" (id, text, list, user_t, "idDeleted") FROM stdin;
 -- Data for Name: Task; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY "Task" (id, list, user_t, text, "time", repeat, "isDone", "isDeleted") FROM stdin;
+COPY "Task" (id, list, user_t, text, "time", repeat, is_done, is_deleted) FROM stdin;
 1	\N	32208604	test1	2016-12-04 23:15:00	\N	0	0
 2	\N	32208604	test2	2016-12-04 23:19:00	\N	0	0
 3	\N	206759060	сделать то-то и то-то	2016-12-12 04:20:00	\N	0	0
@@ -185,11 +186,11 @@ COPY "Task" (id, list, user_t, text, "time", repeat, "isDone", "isDeleted") FROM
 -- Data for Name: User; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY "User" (id, chat, name) FROM stdin;
-1	13	igor
-2	32208604	Igor
-4	273143403	Svyatoslav
-5	206759060	Serebrennikova
+COPY "User" (id, chat, name, user_id) FROM stdin;
+1	13	igor	\N
+2	32208604	Igor	\N
+4	273143403	Svyatoslav	\N
+5	206759060	Serebrennikova	\N
 \.
 
 
